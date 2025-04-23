5518e3ef566670a3768c73ffabc266266a68fccc
lylykin <clelie.jalbert@gmail.com>
2025-04-23 17:26:06 +0200

    feat : worked on the main page

--- tristapp/lib/main.dart
+++ tristapp/lib/main.dart
@@ -18,22 +18,45 @@ class MainApp extends StatelessWidget {
 
 class HomePage extends StatelessWidget{
 
-
   const HomePage({super.key});
+
+  @override
+  Widget build (BuildContext context) {
+
+    return Scaffold(
+      body : BinCard()
+    );
+  } 
+}
+
+class BinCard extends StatelessWidget{
+//issue : I need to resize the card, and to make the icon bigger, or to insert a self-made image for the design.
+
+  const BinCard({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Center (
-      child: Card(
-        child: Column(
-          children: [
-            ListTile(
-              leading: Icon(Icons.delete),
-              title: Text("Sélectionner votre borne"),
-            )
-          ],
+        child: Card(
+          
+          elevation: 20,  //puts shadowing under the card
+          color: Color.fromARGB(255, 149, 214, 151),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10.0),
+          ),
+
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+
+              Text("coucou, je suis un test"),
+              ListTile(
+                leading: Icon(Icons.delete),
+                title: Text("Sélectionner votre borne"),  
+                )
+            ],
+          ),
         ),
-      )
     );
   }
 }
