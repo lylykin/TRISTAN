a85050ad14785c174e2fee1adf9e4100e23c0fdd
lylykin <clelie.jalbert@gmail.com>
2025-05-05 15:08:58 +0200

    new : changed bin page to biselectpage and created mappage

---
+++ tristapp/lib/page/binselectpage.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:tristapp/widget/bincard.dart';
+
+class BinSelectPage extends StatelessWidget {
+  const BinSelectPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BinCard();
+  }
+}
---
+++ tristapp/lib/page/mappage.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MapPage extends StatelessWidget {
+  const MapPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      "coucou, je serai bientôt une map!!! (enfin sauf si clélie a la flemme)",
+    );
+  }
+}
