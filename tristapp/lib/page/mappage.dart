b25ce2a5a63424c589a4689a8c4556c46c53b1db
LePouceBleu <rochedorian26@gmail.com>
2025-05-16 09:28:14 +0200

    add : tentative d'implémenter une carte (abandon)

--- tristapp/lib/page/mappage.dart
+++ tristapp/lib/page/mappage.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:tristapp/widget/mappininfo.dart';
+// import 'package:flutter_map/flutter_map.dart';
+// import 'package:latlong2/latlong.dart';
 
 class MapPage extends StatelessWidget {
   const MapPage({super.key});
@@ -8,9 +10,19 @@ class MapPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Text(
-          "coucou, je serai bientôt une map!!! (enfin sauf si clélie a la flemme)",
-        ),
+        /*
+        FlutterMap(
+          options: MapOptions(
+            initialCenter: LatLng(45.75762368042102, 4.848624249247964), // Centre la map sur Lyon
+            maxZoom: 10
+          ),
+          children: [
+            TileLayer(
+              urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+              userAgentPackageName: 'dev.fleaflet.flutter_map.example',
+            ),
+          ],
+        ),*/
         MapPinInfo(),
       ],
     );
