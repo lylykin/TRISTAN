6f3b6e545634c043b3265d559c62a20e6a93a8ba
lylykin <clelie.jalbert@gmail.com>
2025-05-22 10:55:14 +0200

    fix : converted to int double var to debug

--- tristapp/lib/page/mappage.dart
+++ tristapp/lib/page/mappage.dart
@@ -17,34 +17,44 @@ class _MapPageState extends State<MapPage> {
   Widget build(BuildContext context) {
     final stationList = widget.stationList;
     final double lat = stationList[0].lat;
-    final double long = stationList[0].long; // A remplacer à l'avenir par une attribution automatique des variables selon les différentes bornes données en liste
+    final double long =
+        stationList[0]
+            .long; // A remplacer à l'avenir par une attribution automatique des variables selon les différentes bornes données en liste
     final String stationName = stationList[0].stationName;
 
     MapController mapController = MapController();
 
     return FlutterMap(
-        mapController: mapController,
-        options: MapOptions(
-          initialCenter: LatLng(45.75762368042102, 4.848624249247964), // Centre la map sur Lyon
-          // minZoom: 7,
-          interactionOptions: const InteractionOptions(
-            pinchZoomThreshold: 0.2,
-          )
-          ),
-        children: [
-          TileLayer(
-            urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-            userAgentPackageName: 'com.example.app', // or dev.fleaflet.flutter_map.example maybe ?
-          ),
-          MarkerLayer(markers: [
+      mapController: mapController,
+      options: MapOptions(
+        initialCenter: LatLng(
+          45.75762368042102,
+          4.848624249247964,
+        ), // Centre la map sur Lyon
+        // minZoom: 7,
+        interactionOptions: const InteractionOptions(pinchZoomThreshold: 0.2),
+      ),
+      children: [
+        TileLayer(
+          urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+          userAgentPackageName:
+              'com.example.app', // or dev.fleaflet.flutter_map.example maybe ?
+        ),
+        MarkerLayer(
+          markers: [
             Marker(
               width: 30,
               height: 30,
               point: LatLng(lat, long),
-              child: MapPinInfo(lat: lat, long: long, stationName: stationName,),
-            )
-          ]),
-        ]
-      );
+              child: MapPinInfo(
+                lat: (lat.toInt()),
+                long: (long.toInt()),
+                //key: stationName,
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
   }
 }
