b0668e80effcd17b71d4d8a7cdc1ff02b28b7e81
LePouceBleu <rochedorian26@gmail.com>
2025-05-22 14:57:11 +0200

    edit : reworked the info scroll gui edit : minor fixes

--- tristapp/lib/page/binitempage.dart
+++ tristapp/lib/page/binitempage.dart
@@ -1,22 +1,28 @@
-import 'dart:ffi';
-
 import 'package:flutter/material.dart';
 import 'package:tristapp/widget/returnbutton.dart';
 
-class BinItemPage extends StatelessWidget {
+class BinItemPage extends StatefulWidget {
+  final int? index;
+  
+  const BinItemPage({super.key, this.index});
+
+  @override
+  State<BinItemPage> createState() => _BinItemPageState();
+}
+
+class _BinItemPageState extends State<BinItemPage> {
   List<double?> gpsData = [0.1, 0.2, 0.3];
   List<int?> sparkfunData = [11, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 14];
-  int? index;
-  
-  BinItemPage({super.key, this.index});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Nom de l'objet - Analyse n°$index"),
+        title: Text("Nom de l'objet - Analyse n°${widget.index}"),
         centerTitle: true,
         foregroundColor: Theme.of(context).colorScheme.primary,
+        elevation: 10,
+        scrolledUnderElevation: 50,
         actions: [
           Padding(
             padding: const EdgeInsets.all(8.0),
@@ -28,15 +34,13 @@ class BinItemPage extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Container(
-            child: Expanded(
-              child: ListView.builder(
-                itemCount: 3, // Descriptif, données sparkfun, données gps
-                itemBuilder: (BuildContext context, int index) {
-                  return Text("test $index");
-                },
-              ),
-            )
+          Expanded(
+            child: ListView.builder(
+              itemCount: 3, // Descriptif, données sparkfun, données gps
+              itemBuilder: (BuildContext context, int index) {
+                return Text("test $index");
+              },
+            ),
           ),
         ],
       ),
--- tristapp/lib/widget/itemshow.dart
+++ tristapp/lib/widget/itemshow.dart
@@ -1,23 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:tristapp/page/binitempage.dart';
 
-class ItemShow extends StatelessWidget {
-  int? index;
+class ItemShow extends StatefulWidget {
+  final int? index;
 
-  ItemShow({super.key, this.index});
+  const ItemShow({super.key, this.index});
 
+  @override
+  State<ItemShow> createState() => _ItemShowState();
+}
+
+class _ItemShowState extends State<ItemShow> {
   @override
   Widget build(BuildContext context) {
     return Card(
       child: ListTile(
-        subtitle: Text("Objet analysé n°$index"),
+        subtitle: Text("Objet analysé n°${widget.index}"),
         title: Text("Nom de l'objet"),
         trailing: Text("Appuyer pour afficher les détails"),
-        tileColor: Theme.of(context).colorScheme.surface,
+        tileColor: Theme.of(context).colorScheme.surfaceContainerHigh,
+        shape: RoundedRectangleBorder(
+          side: BorderSide(width: 3, color: Theme.of(context).colorScheme.secondary),
+          borderRadius: BorderRadius.circular(8)
+        ), 
         onTap: () {
           Navigator.push(
             context,
-            MaterialPageRoute(builder: (context) => BinItemPage(index: index)),
+            MaterialPageRoute(builder: (context) => BinItemPage(index: widget.index)),
           );
         },
       ),
