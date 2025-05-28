d9e09a95b5a1b37bebbb33676322008454f8916d
lylykin <clelie.jalbert@gmail.com>
2025-05-28 18:03:58 +0200

    add : added a fucntion to resolve a issue with a dynamic value. Now it need to be implemented in the code (attention dorian j'ai pas debug)

--- tristapp/lib/page/mappage.dart
+++ tristapp/lib/page/mappage.dart
@@ -7,7 +7,8 @@ import 'package:tristapp/data/sensordata.dart';
 import 'package:tristapp/page/binpage.dart';
 
 class MapPage extends StatefulWidget {
-  final List stationList; // liste de tuples de la forme (lat : double, long : double, stationName : String)
+  final List
+  stationList; // liste de tuples de la forme (lat : double, long : double, stationName : String)
   const MapPage({super.key, required this.stationList});
 
   @override
@@ -24,73 +25,87 @@ class _MapPageState extends State<MapPage> {
     final stationList = widget.stationList;
     List<Marker> markersList = [];
 
-    for (int stationIndex = 0; stationIndex < stationList.length; stationIndex++) {
+    for (
+      int stationIndex = 0;
+      stationIndex < stationList.length;
+      stationIndex++
+    ) {
       dynamic lat = stationList[stationIndex].lat;
       dynamic long = stationList[stationIndex].long;
-      markersList.add(Marker( // Création d'un widget marker pour chaque instance de la liste en entrée de mappage
-        width: 30,
-        height: 30,
-        point: LatLng(lat, long),
-        child: FilledButton(
-          onPressed: () {
-            setState(() {
-              if (selectedPinIndex != stationIndex) {
-                selectedPinIndex = stationIndex;
-                pinOverlayController.show(); // Affichage de l'overlay (pin appuyé donc overlay non placeholder)
-              } else {
-                selectedPinIndex = null;
-                pinOverlayController.hide(); // Overlay caché
-              }
-            });
-          },
-          style: FilledButton.styleFrom(
-            padding: EdgeInsets.zero,  // pas de padding interne
+      markersList.add(
+        Marker(
+          // Création d'un widget marker pour chaque instance de la liste en entrée de mappage
+          width: 30,
+          height: 30,
+          point: LatLng(lat, long),
+          child: FilledButton(
+            onPressed: () {
+              setState(() {
+                if (selectedPinIndex != stationIndex) {
+                  selectedPinIndex = stationIndex;
+                  pinOverlayController
+                      .show(); // Affichage de l'overlay (pin appuyé donc overlay non placeholder)
+                } else {
+                  selectedPinIndex = null;
+                  pinOverlayController.hide(); // Overlay caché
+                }
+              });
+            },
+            style: FilledButton.styleFrom(
+              padding: EdgeInsets.zero, // pas de padding interne
+            ),
+            child: Icon(Icons.pin_drop),
+            //key: stationName,
           ),
-          child:  Icon(Icons.pin_drop),
-          //key: stationName,
         ),
-      ),);
+      );
     }
 
     return OverlayPortal(
       controller: pinOverlayController,
       overlayChildBuilder: (context) {
-        if (selectedPinIndex == null) return SizedBox.shrink(); 
-        final station = stationList[selectedPinIndex!]; // Considéré non null car test renvoyant un overlay vide si aucun pin séléctionné
+        if (selectedPinIndex == null) return SizedBox.shrink();
+        final station =
+            stationList[selectedPinIndex!]; // Considéré non null car test renvoyant un overlay vide si aucun pin séléctionné
         return Positioned(
           top: 50,
           left: 150,
           child: Container(
-              decoration: BoxDecoration(
-                color: Theme.of(context).colorScheme.surface,
-                borderRadius: BorderRadius.circular(8),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Column(
-                  children: [
-                    // Besoin de changer les couleurs du texte !
-                    Text(station.stationName.toString(), style: TextStyle(fontWeight: FontWeight.bold),),
-                    Text("Latitude : ${station.lat}\nLongitude : ${station.long}"),
-                    SizedBox(height: 10,),
-                    FilledButton(
-                      onPressed: () {
-                        fetchGpsData(); // Mets l'historique à jour automatiquement
-                        fetchSparkfunData(); // Mets l'historique à jour automatiquement
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => BinPage()),
-                        );
-                      },
-                      child: Text("Accéder à la borne"),
-                    ),
-                  ],
-                ),
+            decoration: BoxDecoration(
+              color: Theme.of(context).colorScheme.surface,
+              borderRadius: BorderRadius.circular(8),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                children: [
+                  // Besoin de changer les couleurs du texte !
+                  Text(
+                    station.stationName.toString(),
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    "Latitude : ${station.lat}\nLongitude : ${station.long}",
+                  ),
+                  SizedBox(height: 10),
+                  FilledButton(
+                    onPressed: () {
+                      fetchGpsData(); // Mets l'historique à jour automatiquement
+                      fetchSparkfunData(); // Mets l'historique à jour automatiquement
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => BinPage()),
+                      );
+                    },
+                    child: Text("Accéder à la borne"),
+                  ),
+                ],
               ),
             ),
+          ),
         );
       },
-      child : FlutterMap(
+      child: FlutterMap(
         mapController: mapController,
         options: MapOptions(
           initialCenter: LatLng(
@@ -104,13 +119,20 @@ class _MapPageState extends State<MapPage> {
           TileLayer(
             urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
             userAgentPackageName:
-              'com.example.app', // or dev.fleaflet.flutter_map.example maybe ?
+                'com.example.app', // or dev.fleaflet.flutter_map.example maybe ?
           ),
-          MarkerLayer(
-            markers: markersList,
-          ),
+          MarkerLayer(markers: markersList),
         ],
-      )
+      ),
     );
   }
+}
+
+double doubleConvert(value) {
+  // function used to convert
+  var type = value.runtimeTime;
+  if (type != double) {
+    value.toInt();
+  }
+  return (value);
 }
